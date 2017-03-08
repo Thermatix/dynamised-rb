26a16f0125fa68806ea6996277f633e1ec502ba2
Martin Becker <mbeckerwork@gmail.com>
2017-03-08 22:34:54 +0000

    adds escape and unescape after methods

--- lib/dynamised/after_scrape_methods.rb
+++ lib/dynamised/after_scrape_methods.rb
@@ -5,6 +5,14 @@ module Dynamised
         string.gsub(/<\/?[^>]*>/, "").strip.gsub(/ ?\\r\\n/,'')
       end
 
+      def unescape_html(string,field_data)
+        CGI::unescapeHTML(string)
+      end
+
+      def escape_html(string,field_data)
+        CGI::escapeHTML(string)
+      end
+
       def page_url(string,field_data)
         @current_url
       end
